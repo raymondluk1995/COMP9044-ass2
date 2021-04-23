1b6b7d89f0b3e47f4647df8bfd8507293f2783eb
Minrui Lu <raymondluk1995@outlook.com>
2021-04-23 15:39:58 +1000

    Update test04.sh

--- test04.sh
+++ test04.sh
@@ -6,22 +6,21 @@ export PATH=$PATH:$(pwd)
 
 mkdir temp 
 cd temp 
-# echo /2/d >  commands.speed
-# echo 4q   >> commands.speed
-# seq 1 5 | speed.pl -f commands.speed
-# echo "-------------------------test 01"
-# echo 5q > commands1
-# seq 1 3 > commands2
-# seq 10 20 > commands3 
-# seq 1 5 | speed.pl -f commands1 commands2 commands3
-# echo "-------------------------test 02"
-# seq 1 2 > two.txt
-# seq 1 5 > five.txt
-# speed.pl '4q;/2/d' two.txt five.txt
-# echo "-------------------------test 03"
-seq 1 100 | speed.pl -n '1,/.1/p;/5/,/9/s/.//;/.{2}/,/.9/p;85q'
-# seq 1 100 | speed.pl -n '1,/.1/p;/.{2}/,/.9/p'
-echo "-------------------------test 04"
+echo /2/d >  commands.speed
+echo 4q   >> commands.speed
+seq 1 5 | speed.pl -f commands.speed
+echo "-------------------------test 01"
+echo 5q > commands1
+seq 1 3 > commands2
+seq 10 20 > commands3 
+seq 1 5 | speed.pl -f commands1 commands2 commands3
+echo "-------------------------test 02"
+seq 1 2 > two.txt
+seq 1 5 > five.txt
+speed.pl '4q;/2/d' two.txt five.txt
+echo "-------------------------test 03"
+# seq 1 100 | speed.pl -n '1,/.1/p;/5/,/9/s/.//;/.{2}/,/.9/p;85q'
+# echo "-------------------------test 04"
 
 
 # seq 1 2 > two.txt
